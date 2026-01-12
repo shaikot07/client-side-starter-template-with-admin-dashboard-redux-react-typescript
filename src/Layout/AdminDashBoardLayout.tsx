f9099de9c5111239df2b8a9eda58f2d3ed12e2d9
shaikot07 <saifulislamshaikat007@gmail.com>
2026-01-13 03:24:10 +0600

    re-factore the admin dashboard layout

--- src/Layout/AdminDashBoardLayout.tsx
+++ src/Layout/AdminDashBoardLayout.tsx
@@ -24,14 +24,8 @@ import {
 } from "@heroicons/react/24/outline";
 import { Fragment, useEffect, useState } from "react";
 import { FaUsers } from "react-icons/fa";
-import { ArrowLeft, ArrowRight } from "lucide-react";
-import {
-  Link,
-  NavLink,
-  Outlet,
-  useLocation,
-  useNavigate,
-} from "react-router-dom";
+import { NavLink, Outlet, useLocation, useNavigate } from "react-router-dom";
+import DashboardSideBar from "@/component/DashboardSideBar/DashboardSideBar";
 
 function AdminDashBoardLayout() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -95,66 +89,6 @@ function AdminDashBoardLayout() {
     </NavLink>
   );
 
-  const SidebarContent = ({
-    collapsed = false,
-    onItemClick,
-  }: {
-    collapsed?: boolean;
-    onItemClick: () => void;
-  }) => (
-    <div className="flex h-full flex-col  ">
-      <Link
-        to={"/"}
-        className={`flex items-center justify-center bg-white ${
-          collapsed ? "px-1 py-3" : "px-3 py-2"
-        }`}
-      >
-        <h1
-          className={`transition-all duration-300 text-center tektur-font font-bold text-[#4881FF] mb-2 lg:mb-2
-      ${collapsed ? "text-sx lg:text-xl" : "text-3xl lg:text-3xl"}
-    `}
-        >
-          Covermate
-        </h1>
-      </Link>
-
-      {/*  Show Collapse OR Expand button based on state */}
-      <div className="hidden border-t   p-3 lg:block">
-        {!collapsed ? (
-          <button
-            onClick={() => setIsCollapsed(true)}
-            className="flex w-full items-center justify-between text-white rounded-lg px-3 py-2 text-sm font-medium  transition-colors cursor-pointer "
-            aria-label="Collapse sidebar"
-          >
-            <span>Hide Menu</span>
-            <ArrowLeft />
-          </button>
-        ) : (
-          <button
-            onClick={() => setIsCollapsed(false)}
-            className="flex w-full items-center gap-3 cursor-pointer rounded-lg px-3 py-2 text-sm font-medium  text-white transition-colors  "
-            aria-label="Expand sidebar"
-          >
-            <ArrowRight />
-          </button>
-        )}
-      </div>
-
-      <nav className="flex-1 overflow-y-auto px-3 py-6 ">
-        <div className="space-y-3">
-          {navLinks.map((link) => (
-            <NavItem
-              key={link.to}
-              link={link}
-              onClick={onItemClick}
-              collapsed={collapsed}
-            />
-          ))}
-        </div>
-      </nav>
-    </div>
-  );
-
   return (
     <div className="flex h-screen overflow-hidden ">
       {sidebarOpen && (
@@ -191,7 +125,14 @@ function AdminDashBoardLayout() {
                   />
                 </button>
               </div>
-              <SidebarContent onItemClick={() => setSidebarOpen(false)} />
+              {/* here is for mobile  */}
+              {/* <SidebarContent onItemClick={() => setSidebarOpen(false)} /> */}
+              <DashboardSideBar
+                onItemClick={() => setSidebarOpen(false)}
+                setIsCollapsed={setIsCollapsed}
+                navLinks={navLinks}
+                NavItem={NavItem}
+              />
             </div>
           </Transition.Child>
         </div>
@@ -203,7 +144,14 @@ function AdminDashBoardLayout() {
         }`}
       >
         <div className="flex w-full flex-col border-r border-gray-200 bg-[#353535] shadow-md">
-          <SidebarContent collapsed={isCollapsed} onItemClick={() => {}} />
+          {/* <SidebarContent collapsed={isCollapsed} onItemClick={() => {}} /> */}
+          <DashboardSideBar
+            collapsed={isCollapsed}
+            onItemClick={() => {}}
+            setIsCollapsed={setIsCollapsed}
+            navLinks={navLinks}
+            NavItem={NavItem}
+          />
         </div>
       </div>
 
